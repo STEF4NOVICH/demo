bb21c96d3bd93d112689339bae92b7845cd0b3d6
Casper Poulsen <casperarp3777@gmail.com>
2021-04-06 11:02:53 +0200

    sletdenher

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
 <div>
     <?php
 
-    include('header.php');
+    include('portal-login/header.php');
 
     require('db.php');
     session_start();
@@ -51,9 +51,6 @@
                 <input type="password" name="password" placeholder="Kodeord" />
                 <input type="submit" value="Login" name="submit" />
             </form>
-
-
-
 
         </section>
 
