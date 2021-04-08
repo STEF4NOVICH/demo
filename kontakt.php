65f053bc838e9961f3d3eb278eb4720d7cd3cc20
kimx965q <kimnguyen@live.dk>
2021-04-08 20:40:43 +0200

    kontakside næsten færdig

--- kontakt.php
+++ kontakt.php
@@ -9,113 +9,186 @@ include('header.php');
 
 <section class="site-wrapper">
     <div class="stripe">
-        <div class="kontakt-os">
+        <div class="kontakt">
             <h1 class="overskrift">Kontakt os</h2>
         </div>
     </div>
-
-    <div class="top-content">
-        <div class="top-container-left">
-            <h2>Berit's hjemmepleje kan kontaktes her:</h2>
-            <p>Telefontid angående praktisk hjælp eller
-                generelle henvendelser: 8-15 på hverdage
-            </p>
-            <div class="phone-cta">
-                <i class="fas fa-mobile-alt"></i>
-                <a href="#">+45 93 63 99 99</a>
-            </div>
-            <p>Der besvares ikke på SMS beskeder til
-                telefonnummeret! <br><br>
-
-                Telefontid angående personlig pleje:
-                Døgnbemandet telefonvagt.<br><br>
-
-                Telefonnummer fremgår af udleverede
-                papirer.<br><br>
-
-                Adresse: (Kontoret er kun åbent for
-                henvendelser efter aftale)</p>
-        </div>
-        <div class="top-container-right">
-            <img src="billeder/Beritprofilfote.jpg" alt="">
-            <h3>Berit B. Thomsen</h3>
-            <div class="stilling">
-                <i class="fas fa-street-view"></i>
-                <p>Stifter/ Leder</p>
-            </div>
-            <div class="mail">
-                <i class="far fa-envelope"></i>
-                <p>bbt@berits-hjemmepleje.dk</p>
-            </div>
-        </div>
-    </div>
-
-
-    <div class="aarhus-container">
-        <h1>Århus holdet</h1>
-
-        <div class="r1">
-            <div class="profiles">
-                <div class="profile">
-                    <img src="billeder/Tine-nielsen.png" alt="" class="profile-img">
-
-                    <h3 class="user-name">Tine Nielsen</h3>
-                    <h5>Leder af rengøringsafdeling</h5>
-                    <p>tn@berits-hjemmepleje.dk</p>
-                </div>
-            </div>
-
-            <div class="profiles">
-                <div class="profile">
-                    <img src="billeder/Lisa.png" alt="" class="profile-img">
-
-                    <h3 class="user-name">Lisa S. Stengaard</h3>
-                    <h5>Planlægger</h5>
-                    <p>ls@berits-hjemmepleje.dk</p>
-
-                </div>
-            </div>
-            <div class="profiles">
-                <div class="profile">
-                    <img src="billeder/Michelle_t..png" alt="" class="profile-img">
-
-                    <h3 class="user-name">Michelle Thomsen</h3>
-                    <h5>Sygeplejeske Århus</h5>
-                    <p>mt@berits-hjemmepleje.dk</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="r2">
-            <div class="profiles">
-                <div class="profile">
-                    <img src="billeder/Annette_S.png" alt="" class="profile-img">
-
-                    <h3 class="user-name">Annette Søgaard</h3>
-                    <h5>Administration</h5>
-                    <p>as@berits-hjemmepleje.dk </p>
-                </div>
-            </div>
-
-            <div class="profiles1">
-                <div class="profile">
-                    <img src="billeder/billede_påvej.png" alt="" class="profile-img">
-
-                    <h3 class="user-name">Else-Marie L. Frandsen</h3>
-                    <h5>Sygeplejeske Århus</h5>
-                    <p>emf@berits-hjemmepleje.dk</p>
-                </div>
-            </div>
-
-
-
-        </div>
-
-    </div>
-
-
 </section>
 
+<section class="intro-kontakt">
+    <div class="intro-container">
+        <ul>
+            <li class="transition">
+                <h2 class="heading1">Berit's hjemmepleje kan kontaktes her:</h2>
+                <p>Telefontid angående praktisk hjælp eller
+                    generelle henvendelser: 8-15 på hverdage</p>
+                <div class="phone-cta">
+                    <i class="fas fa-mobile-alt"></i>
+                    <a href="#">+45 63 93 99 99</a>
+                </div>
+                <p>Der besvares ikke på SMS beskeder til
+                    telefonnummeret! <br><br>
+
+                    Telefontid angående personlig pleje:
+                    Døgnbemandet telefonvagt.<br><br>
+
+                    Telefonnummer fremgår af udleverede
+                    papirer.<br><br>
+
+                    Adresse: (Kontoret er kun åbent for
+                    henvendelser efter aftale)</p>
+            </li>
+
+            <li class="transition">
+                <div class="img-container one">
+                    <img src="billeder/Beritprofilfote.jpg" alt="Billede af  stifteren Berit">
+                </div>
+                <p class="name">Berit B. Thomsen</p>
+                <p class="stilling">Stifter/Leder</p>
+                <a href="#" class="mail">bbt@berits-hjemmepleje.dk </a>
+            </li>
+        </ul>
+    </div>
+</section>
+
+<section class="aarhus">
+    <h1 class="heading2"> Vores hold i Århus</h1>
+    <div class="aarhus-container">
+        <ul>
+            <li class="transition2">
+                <div class="img-container two">
+                    <img src="billeder/Tine-nielsen.png" alt="Billede af Tine">
+                </div>
+                <p class="name">Tine Nielsen</p>
+                <p class="stilling">Leder af rengøringsafdeling</p>
+                <a href="#" class="mail">tn@berits-hjemmepleje.dk</a>
+            </li>
+
+            <li class="transition2">
+                <div class="img-container two">
+                    <img src="billeder/Lisa.png" alt="Billede af Tine">
+                </div>
+                <p class="name">Lisa S. Stengaard</p>
+                <p class="stilling">Planlægger</p>
+                <a href="#" class="mail">ls@berits-hjemmepleje.dk </a>
+            </li>
+
+            <li class="transition2">
+                <div class="img-container two">
+                    <img src="billeder/Michelle_t..png" alt="Billede af Tine">
+                </div>
+                <p class="name">Michelle Thomsen</p>
+                <p class="stilling">Sygeplejeske Århus</p>
+                <a href="#" class="mail">mic@berits-hjemmepleje.dk </a>
+            </li>
+
+            <li class="transition2">
+                <div class="img-container two">
+                    <img src="billeder/Annette_S.png" alt="Billede af Tine">
+                </div>
+                <p class="name">Annette Søgaard</p>
+                <p class="stilling">Administration</p>
+                <a href="#" class="mail">as@berits-hjemmepleje.dk </a>
+            </li>
+
+            <li class="transition2">
+                <div class="img-container two">
+                    <img src="billeder/billede_påvej.png" alt="Billede af Tine">
+                </div>
+                <p class="name">Else-marie L. Frandsen</p>
+                <p class="stilling">Kundeservice og Kvalitetssikring</p>
+                <a href="#" class="mail">emf@berits-hjemmepleje.dk </a>
+            </li>
+        </ul>
+    </div>
+</section>
+
+
+<div class="stripe-left">
+    <div class="kontor-og-base">
+        <i class="fas fa-map-marker-alt"></i>
+        <div class="arhus-adr">
+            <p class="adr-title">Hovedkontor Århus</p>
+            <p>Klamsagervej 19 B 230 Åbyhøj</p>
+        </div>
+        <div class="arhus-adr">
+            <p class="adr-title">Lokalkontor Århus Nord</p>
+            <p>Sindalsvej 30 8240 Risskov</p>
+        </div>
+    </div>
+</div>
+
+<section class="horsens">
+    <h1 class="heading3"> Vores hold i Horsens og Hedensted</h1>
+    <div class="hh-container">
+        <ul>
+            <li class="transition2">
+                <div class="img-container two">
+                    <img src="billeder/Heidi.png" alt="Billede af Heidi">
+                </div>
+                <p class="name">Heidi K. Hansen</p>
+                <p class="stilling">Souschef</p>
+                <a href="#" class="mail">hh@berits-hjemmepleje.dk</a>
+            </li>
+
+            <li class="transition2">
+                <div class="img-container two">
+                    <img src="billeder/Karina_Y.png" alt="Billede af Karina Yde">
+                </div>
+                <p class="name">Karina A. Yde</p>
+                <p class="stilling">Planlægger</p>
+                <a href="#" class="mail">ky@berits-hjemmepleje.dk</a>
+            </li>
+
+            <li class="transition2">
+                <div class="img-container two">
+                    <img src="billeder/Karina_E.png" alt="Billede af Karina Eisner">
+                </div>
+                <p class="name">Karina Eisner</p>
+                <p class="stilling">Sygeplejeske Horsens</p>
+                <a href="#" class="mail">kae@berits-hjemmepleje.dk</a>
+            </li>
+
+            <li class="transition2">
+                <div class="img-container two">
+                    <img src="billeder/Lone.png" alt="Billede af Lone">
+                </div>
+                <p class="name">Lone M. Mikkelsen</p>
+                <p class="stilling">Administration</p>
+                <a href="#" class="mail">lm@berits-hjemmepleje.dk </a>
+            </li>
+
+            <li class="transition2">
+                <div class="img-container two">
+                    <img src="billeder/michael.png" alt="Billede af Tine">
+                </div>
+                <p class="name">Michael Thomsen</p>
+                <p class="stilling">Økonomi & teknik</p>
+                <a href="#" class="mail">mlt@berits-hjemmepleje.dk</a>
+            </li>
+        </ul>
+    </div>
+</section>
+
+<div class="stripe-left">
+    <div class="kontor-og-base">
+        <i class="fas fa-map-marker-alt"></i>
+        <div class="arhus-adr">
+            <p class="adr-title">Hovedkontor Horsens</p>
+            <p>Norgesvej 5, 8700, Horsens</p>
+        </div>
+        <div class="arhus-adr">
+            <p class="adr-title">Lokalkontor Brædstrup</p>
+            <p>Søndergade 27, 8740, Brædstrup</p>
+        </div>
+        <div class="arhus-adr">
+            <p class="adr-title">Lokalkontor Hedensted</p>
+            <p>Kildeparken 12, 8722, Hedensted</p>
+        </div>
+    </div>
+</div>
+
+</section>
 
 <?php
 include('footer.php');
