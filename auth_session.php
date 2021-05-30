45536949964ff374008dfe84dbbd40326f4279ed
Casper Poulsen <70193177+STEF4NOVICH@users.noreply.github.com>
2021-05-30 23:54:40 +0200

    Update auth_session.php

--- auth_session.php
+++ auth_session.php
@@ -1,13 +1,13 @@
 <?php
 
-/* Next, we have to create a session for the user. Create a file named
-auth_session.php and paste the codes below. */
+
 session_start();
 if (!isset($_SESSION["username"])) {
     header("Location: login.php");
     exit();
 }
 ?>
+
 </body>
 
 </html>
